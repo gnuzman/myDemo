177d68680dedab8b84e5f29a2da2bfb400ac7f78
root <94644378@qq.com>
2017-11-26 11:44:56 +0800

    json

--- src/main/java/com/zte/demo/TestController.java
+++ src/main/java/com/zte/demo/TestController.java
@@ -1,11 +1,20 @@
 package com.zte.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+
+@Api("TestController")
 @RestController
 public class TestController {
 
+	@PostMapping("/post")
+	public String testPost() {
+		return "post";
+	}
+	
 	@GetMapping("test")
 	public String test() {
 		return "test";
